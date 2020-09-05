bdd8e8e6861c45d8c7833ff693614f335c431b63
Jarrod Merryman <jarrodmerryman@gmail.com>
2020-09-05 15:54:59 -0500

    adds strip to run then turn off

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+# LED strip configuration:
+LED_COUNT = 43        # Number of LED pixels.
+LED_PIN = 12          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+def strip_on('strip'):
+	strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    rainbowCycle(strip)
+    colorWipe(strip, Color(0, 0, 0), 10)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
